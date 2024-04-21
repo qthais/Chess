f88c055bb307398339cc3c8b3f4eab4a34c656a6
qthais <thaiallb63@gmail.com>
2024-04-21 16:55:59 +0700

    Knight moves

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -1,29 +1,28 @@
 package com.chess.engine.board;
 import java.util.*;
 import com.chess.engine.pieces.Piece;
-
 public abstract class Tile {
     protected final int tileCoordinate;
-    private  static final Map<Integer,EmptyTile> EMPTY_TILES= createAllPossibleEmptyTiles();
+    private  static final Map<Integer,EmptyTile> EMPTY_TILES_CACHE= createAllPossibleEmptyTiles();
 
     private static Map<Integer,EmptyTile> createAllPossibleEmptyTiles() {
         final Map<Integer,EmptyTile> emptyTileMap=new HashMap<>();
         for(int i=0;i<64;i++){
             emptyTileMap.put(i,new EmptyTile(i));
         }
-        return  emptyTileMap;
+        return  Collections.unmodifiableMap(emptyTileMap);
     }
 
     private Tile(int tileCoordinate){
         this.tileCoordinate=tileCoordinate;
     }
     public static Tile createTile(final int tileCoordinate,final Piece piece){
-        return piece!=null ? new OccupiedTile(tileCoordinate,piece) : EMPTY_TILES.get(tileCoordinate);
+        return piece!=null ? new OccupiedTile(tileCoordinate,piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
     }
     public abstract boolean isTileOccupied();
     public abstract Piece getPiece();
     public static final class EmptyTile extends Tile{
-        EmptyTile(final int coordinate){
+        private EmptyTile(final int coordinate){
             super(coordinate);
         }
 
@@ -39,7 +38,7 @@ public abstract class Tile {
     }
     public static final class OccupiedTile extends Tile{
         private final Piece pieceOnTile;
-        OccupiedTile(int tileCoordinate,Piece pieceOnTile){
+        private OccupiedTile(int tileCoordinate,Piece pieceOnTile){
             super(tileCoordinate);
             this.pieceOnTile=pieceOnTile;
         }
--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,4 +1,21 @@
 package  com.chess.engine.pieces;
-public class Piece {
-    
+import java.util.List;
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+public abstract class Piece {
+    protected final int piecePosition;
+    protected  final Alliance pieceAlliance;
+    Piece(final int  piecePosition, final Alliance pieceAliiance){
+        this.piecePosition=piecePosition;
+        this.pieceAlliance=pieceAliiance;
+    }
+
+    public Alliance getPieceAlliance() {
+        return pieceAlliance;
+    }
+
+    public abstract List<Move> calculateLegalMoves(final Board board);
+
 }
