d0b9926f0a8dbacbea549c9c11282d67c5b8792b
qthais <thaiallb63@gmail.com>
2024-04-22 12:40:42 +0700

    The Queen

--- src/com/chess/engine/pieces/Queen.java
+++ src/com/chess/engine/pieces/Queen.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class Rook extends Piece{
-    private final static int[] CANDIDATE_VECTOR_MOVE_COORDINATES={-8,-1,1,8};
-    Rook(int piecePosition, Alliance pieceAlliance) {
+public class Queen extends Piece{
+    private final static int[] CANDIDATE_VECTOR_MOVE_COORDINATES={-9,-7,7,9,-8,-1,1,8};
+    Queen(int piecePosition, Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
 
@@ -43,10 +43,10 @@ public class Rook extends Piece{
     }
     private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
         return BoardUtils.FIRST_COLUMN[currentPosition]
-                && (candidateOffset == -1 );
+                && (candidateOffset == -9 || candidateOffset == 7|| candidateOffset == -1);
     }
     private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset) {
         return BoardUtils.EIGHTH_COLUMN[currentPosition]
-                && (candidateOffset == 1);
+                && (candidateOffset == -7 || candidateOffset == 9|| candidateOffset == 1);
     }
 }
