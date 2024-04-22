62aa4bebc9babc91371677bfda5529486a556c20
qthais <thaiallb63@gmail.com>
2024-04-22 12:13:45 +0700

    Modify The Bishop

--- src/com/chess/engine/pieces/Bishop.java
+++ src/com/chess/engine/pieces/Bishop.java
@@ -1,11 +1,8 @@
 package com.chess.engine.pieces;
 
 import com.chess.engine.Alliance;
-import com.chess.engine.board.Board;
-import com.chess.engine.board.BoardUtils;
-import com.chess.engine.board.Move;
-import com.chess.engine.board.Tile;
-
+import com.chess.engine.board.*;
+import com.chess.engine.board.Move.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -30,12 +27,12 @@ public class Bishop extends Piece{
                 if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
                     final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
                     if (!candidateDestinationTile.isTileOccupied()) {
-                        legalMoves.add(new Move.MajorMove(board,this,candidateDestinationCoordinate));
+                        legalMoves.add(new MajorMove(board,this,candidateDestinationCoordinate));
                     } else {
                         final Piece pieceAtDestination = candidateDestinationTile.getPiece();
                         final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
                         if (this.pieceAlliance != pieceAlliance) {
-                            legalMoves.add(new Move.AttackMove(board,this,candidateDestinationCoordinate,pieceAtDestination));
+                            legalMoves.add(new AttackMove(board,this,candidateDestinationCoordinate,pieceAtDestination));
                         }
                         break;
                     }
